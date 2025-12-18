792041a6ce19a40cf3b07c2298ddece8465f1991
Kakha <kakhaber.toklikishvili@gmail.com>
2025-12-18 15:51:56 +0400

    Added addCarter in program.cs

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
