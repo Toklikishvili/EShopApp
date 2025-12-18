bc4d51eed19bc9e3a08ad8d0e6344e2aff6d97db
Kakha <kakhaber.toklikishvili@gmail.com>
2025-12-18 15:15:29 +0400

    Added logic in CreateProductHadler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHadler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHadler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -7,15 +9,30 @@ public record CreateProductCommand(
     List<string> Category,
     string Description,
     string ImageFile,
-    decimal Price) : IRequest<CreateProductResult>;
+    decimal Price) : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductHadler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductHadler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // Business logic to create a product would go here.
-        throw new NotImplementedException();
+        // create Product entity from command object
+        var product = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //TODO
+        // save entity to database
+
+        // return CreateProductResult result
+
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
